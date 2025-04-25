3bf69337bdc58a0ae216543e14505577d755439b
Devika Shaj Kumar Nair <69066885+Devika-S-N@users.noreply.github.com>
2025-04-25 13:53:55 -0700

    Adding modified dataset file

---
+++ scripts/modified_dataset.py
@@ -0,0 +1,26 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision.transforms import functional as TF
+
+class SRDataset_modified(Dataset):
+    def __init__(self, lr_dir, crop_size=96, upscale=4):
+        self.lr_dir = lr_dir
+        self.crop_size = crop_size
+        self.upscale = upscale
+        self.filenames = sorted(os.listdir(self.lr_dir))
+
+    def __len__(self):
+        return len(self.filenames)
+
+    def __getitem__(self, idx):
+        lr_path = os.path.join(self.lr_dir, self.filenames[idx])
+        lr_img = Image.open(lr_path).convert("RGB")
+
+        # Get center crop of LR 
+        lr_crop = TF.center_crop(lr_img, (self.crop_size, self.crop_size))
+
+        # Convert to tensors
+        lr_tensor = TF.to_tensor(lr_crop)
+
+        return lr_tensor
